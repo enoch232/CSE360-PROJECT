acf8da3c3097370c2cc07e912cca09e26fd1498b
TooSchoolForCool <zeyuz@outlook.com>
2016-04-12 15:41:08 -0700

    Implement baisc functions for Dice class

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,14 @@
+import java.util.Random;
 
 public class Dice {
-
+	private Random rand;
+	
+	Dice(){
+		rand = new Random();
+	}
+	
+	public int randInt(int min, int max){
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+	    return randomNum;
+	}
 }
