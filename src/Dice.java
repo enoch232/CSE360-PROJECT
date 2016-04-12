c8dd87bb8151034ec807b89d9a7f48bff880cb88
TooSchoolForCool <zeyuz@outlook.com>
2016-04-12 16:12:05 -0700

    Implemetn Basic Functions for Player Class

--- src/Dice.java
+++ src/Dice.java
@@ -1,14 +1,14 @@
 import java.util.Random;
 
 public class Dice {
-	private Random _rand;
+	private Random rand;
 	
 	Dice(){
-		_rand = new Random();
+		rand = new Random();
 	}
 	
 	public int randInt(int min, int max){
-		int randomNum = _rand.nextInt((max - min) + 1) + min;
+		int randomNum = rand.nextInt((max - min) + 1) + min;
 		return randomNum;
 	}
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,55 @@
 
 public class Player {
+	private String name;
+	private int balance;
+	private int points;
+	private boolean isPass;
+	private Dice dice;
 	
+	private void initialize(String newName, int newBalance){
+		name = newName;
+		balance = newBalance;
+		points = 0;
+		isPass = false;
+		dice = new Dice();
+	}
+	
+	Player(String newName, int newBalance){
+		initialize(newName, newBalance);
+	}
+	
+	Player(String newName){
+		initialize(newName, 1000);
+	}
+	
+	public int checkBalance(){
+		return balance;
+	}
+	
+	public int getCurPoints(){
+		return points;
+	}
+	
+	public String getPlayerName(){
+		return name;
+	}
+	
+	public int rollDice(){
+		int val = dice.randInt(1, 6);
+		return val;
+	}
+	
+	public void pass(){
+		isPass = true;
+	}
+	
+	public boolean checkPass(){
+		return isPass;
+	}
+	
+	public void createNewRound(){
+		points = 0;
+		isPass = false;
+	}
 }
+
