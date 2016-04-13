1fbf22b997f58c30b72f2b511ceb920ab1e8251d
TooSchoolForCool <zeyuz@outlook.com>
2016-04-12 21:29:00 -0700

    Add two functions(addPoints, printInfo) in the Player Class

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,13 @@
 
 public class Player {
-	private String name;
+	private String userName;
 	private int balance;
 	private int points;
 	private boolean isPass;
 	private Dice dice;
 	
 	private void initialize(String newName, int newBalance){
-		name = newName;
+		userName = newName;
 		balance = newBalance;
 		points = 0;
 		isPass = false;
@@ -26,6 +26,38 @@ public class Player {
 		initialize("", 100);
 	}
 	
+	public void setPlayerName(String newName){
+		userName = newName;
+	}
+	
+	public void setBalance(int newBalance){
+		balance = newBalance;
+	}
+	
+	public void addPoints(int val){
+		points += val;
+	}
+	
+	public void createNewRound(){
+		points = 0;
+		isPass = false;
+	}
+	
+	public void pass(){
+		isPass = true;
+	}
+	
+	public int rollDice(){
+		int val = dice.randInt(1, 6);
+		return val;
+	}
+	
+	public void printInfo(){
+		System.out.printf("Username:\t\t%s\n", userName);
+		System.out.printf("Balance:\t\t%d\n", balance);
+		System.out.printf("Points:\t\t%d\n", points);
+	}
+	
 	public int checkBalance(){
 		return balance;
 	}
@@ -35,33 +67,10 @@ public class Player {
 	}
 	
 	public String getPlayerName(){
-		return name;
-	}
-	
-	public int rollDice(){
-		int val = dice.randInt(1, 6);
-		return val;
-	}
-	
-	public void pass(){
-		isPass = true;
+		return userName;
 	}
 	
 	public boolean checkPass(){
 		return isPass;
 	}
-	
-	public void createNewRound(){
-		points = 0;
-		isPass = false;
-	}
-	
-	public void setPlayerName(String newName){
-		name = newName;
-	}
-	
-	public void setBalance(int newBalance){
-		balance = newBalance;
-	}
 }
-
