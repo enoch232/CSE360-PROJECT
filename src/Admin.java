ed4c79e5571bf265b7e20fffdf9cdea97dca5d34
TooSchoolForCool <zeyuz@outlook.com>
2016-04-13 09:32:42 -0700

    Fix the startNewGame function in Admin Class

--- src/Admin.java
+++ src/Admin.java
@@ -12,7 +12,12 @@ public class Admin {
         
         in.nextLine();  // Dump the character '\n'
         
+        // System.out.printf("numOfPlayer: %d\n", numOfPlayers);
         playerArr = new Player[numOfPlayers];
+        for(int i = 0; i < numOfPlayers; i++)
+        	playerArr[i] = new Player();
+        // System.out.printf("Creater Players Array.\n");
+        
         
         for(int i = 0; i < numOfPlayers; i++){
             System.out.printf("Please enter player %d's name: ", i + 1);
@@ -22,9 +27,11 @@ public class Admin {
             newBalance = in.nextInt();
             
             in.nextLine();  // Dump the character '\n'
-            
+            // System.out.printf("newName: %s\tnewBalance: %d\n", "asd", newBalance);
+          
             playerArr[i].setPlayerName(newName);
             playerArr[i].setBalance(newBalance);
+            // System.out.printf("Success\n");
         }
         
         in.close();
