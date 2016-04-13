5b3b0dff995a368a80345dbfb7fb8658d81b712a
TooSchoolForCool <zeyuz@outlook.com>
2016-04-12 21:08:58 -0700

    Add DiceGame Class

---
+++ src/DiceGame.java
@@ -0,0 +1,14 @@
+
+public class DiceGame {
+    private int numOfPlayers;
+    private Player[] playerArr;
+    private Admin admin;
+    
+    DiceGame(){
+        admin = new Admin();
+    }
+    
+    public void runGame(){
+        
+    }
+}
