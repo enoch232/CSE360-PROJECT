819f2445e729e6e738c2c61fefc5161c807559bf
TooSchoolForCool <zeyuz@outlook.com>
2016-04-15 00:51:55 -0700

    Add one more element in class RankRecord

--- src/RankRecord.java
+++ src/RankRecord.java
@@ -2,6 +2,11 @@
 public class RankRecord {
     private String winnerName;
     private int winnerPoints;
+    private boolean isTie;
+    
+    RankRecord(){
+        isTie = false;
+    }
     
     public void setWinnerName(String name){
         winnerName = name;
@@ -11,11 +16,19 @@ public class RankRecord {
         winnerPoints = points;
     }
     
+    public void setTie(){
+        isTie = true;
+    }
+    
     public String getWinnerName(){
         return winnerName;
     }
     
     public int getWinnerPoints(){
         return winnerPoints;
+    }
+    
+    public boolean isTie(){
+        return isTie;
     }
 }
