1aba1d1ef6f7e7a780237fe7dec4834c562ea330
TooSchoolForCool <zeyuz@outlook.com>
2016-04-14 23:49:17 -0700

    Add new class RankRecord

---
+++ src/RankRecord.java
@@ -0,0 +1,21 @@
+
+public class RankRecord {
+    private String winnerName;
+    private int winnerPoints;
+    
+    public void setWinnerName(String name){
+        winnerName = name;
+    }
+    
+    public void setWinnerPoints(int points){
+        winnerPoints = points;
+    }
+    
+    public String getWinnerName(){
+        return winnerName;
+    }
+    
+    public int getWinnerPoints(){
+        return winnerPoints;
+    }
+}
