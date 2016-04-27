ca1dd46d62ebc474d1b96ffec5ce9c88e04da1e9
TooSchoolForCool <zeyuz@outlook.com>
2016-04-26 23:09:12 -0700

    Design RunGamePage layout

--- DiceGameApp/src/dicegameapp/GameRunPage.java
+++ DiceGameApp/src/dicegameapp/GameRunPage.java
@@ -32,23 +32,198 @@ public class GameRunPage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        betButton = new javax.swing.JButton();
+        passButton = new javax.swing.JButton();
+        rollDiceButton = new javax.swing.JButton();
+        quitButton = new javax.swing.JButton();
+        rankingButton = new javax.swing.JButton();
+        diceRandomLabel = new javax.swing.JLabel();
+        diceOneLabel = new javax.swing.JLabel();
+        diceTwoLabel = new javax.swing.JLabel();
+        diceThreeLabel = new javax.swing.JLabel();
+        diceFourLabel = new javax.swing.JLabel();
+        diceFiveLabel = new javax.swing.JLabel();
+        diceSixLabel = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        usernameLabel = new javax.swing.JLabel();
+        balanceLabel = new javax.swing.JLabel();
+        scoreLabel = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        betButton.setText("BET");
+        betButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                betButtonActionPerformed(evt);
+            }
+        });
+
+        passButton.setText("PASS");
+        passButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                passButtonActionPerformed(evt);
+            }
+        });
+
+        rollDiceButton.setText("Roll Dice");
+        rollDiceButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rollDiceButtonActionPerformed(evt);
+            }
+        });
+
+        quitButton.setText("Quit");
+
+        rankingButton.setText("Ranking");
+
+        diceRandomLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picSrc/dice_random.png"))); // NOI18N
+        diceRandomLabel.setInheritsPopupMenu(false);
+
+        diceOneLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picSrc/dice_1.png"))); // NOI18N
+
+        diceTwoLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picSrc/dice_2.png"))); // NOI18N
+
+        diceThreeLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picSrc/dice_3.png"))); // NOI18N
+
+        diceFourLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picSrc/dice_4.png"))); // NOI18N
+
+        diceFiveLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picSrc/dice_5.png"))); // NOI18N
+
+        diceSixLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picSrc/dice_6.png"))); // NOI18N
+
+        jLabel2.setText("balance:");
+
+        jLabel3.setText("score:");
+
+        usernameLabel.setText("jLabel4");
+
+        balanceLabel.setText("jLabel4");
+
+        scoreLabel.setText("jLabel4");
+
+        jLabel4.setText("username:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 480, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(56, 56, 56)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(usernameLabel))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel3))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(scoreLabel)
+                                    .addComponent(balanceLabel))))
+                        .addGap(174, 174, 174)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(passButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(rankingButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(rollDiceButton, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(quitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(betButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(187, 187, 187)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(diceFourLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(diceSixLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(diceOneLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(diceRandomLabel)
+                            .addComponent(diceThreeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(diceTwoLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(diceFiveLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(54, Short.MAX_VALUE))
         );
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {betButton, passButton, quitButton, rankingButton, rollDiceButton});
+
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 357, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(44, 44, 44)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(diceFourLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(diceSixLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(diceOneLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(diceRandomLabel)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(diceThreeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(diceTwoLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(diceFiveLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rankingButton)
+                    .addComponent(passButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel4)
+                    .addComponent(usernameLabel))
+                .addGap(1, 1, 1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rollDiceButton)
+                    .addComponent(betButton)
+                    .addComponent(jLabel2)
+                    .addComponent(balanceLabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(quitButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel3)
+                        .addComponent(scoreLabel)))
+                .addContainerGap(59, Short.MAX_VALUE))
         );
+
+        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {betButton, passButton, quitButton, rankingButton, rollDiceButton});
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void rollDiceButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rollDiceButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_rollDiceButtonActionPerformed
+
+    private void betButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_betButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_betButtonActionPerformed
+
+    private void passButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_passButtonActionPerformed
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel balanceLabel;
+    private javax.swing.JButton betButton;
+    private javax.swing.JLabel diceFiveLabel;
+    private javax.swing.JLabel diceFourLabel;
+    private javax.swing.JLabel diceOneLabel;
+    private javax.swing.JLabel diceRandomLabel;
+    private javax.swing.JLabel diceSixLabel;
+    private javax.swing.JLabel diceThreeLabel;
+    private javax.swing.JLabel diceTwoLabel;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JButton passButton;
+    private javax.swing.JButton quitButton;
+    private javax.swing.JButton rankingButton;
+    private javax.swing.JButton rollDiceButton;
+    private javax.swing.JLabel scoreLabel;
+    private javax.swing.JLabel usernameLabel;
     // End of variables declaration//GEN-END:variables
 }
--- src/Tester.java
+++ src/Tester.java
@@ -5,3 +5,4 @@ public class Tester {
 		game.runGame();
 	}
 }
+
