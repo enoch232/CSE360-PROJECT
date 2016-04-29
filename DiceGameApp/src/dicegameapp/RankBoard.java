212e62e086778b5337aa89c9b4391fade65d8d89
Yanyao Wang <wangyanyao@msn.com>
2016-04-28 22:05:41 -0700

    Add pop up Rank Report

--- DiceGameApp/src/dicegameapp/RankBoard.java
+++ DiceGameApp/src/dicegameapp/RankBoard.java
@@ -42,33 +42,43 @@ public class RankBoard {
     }
     
     public void printRankBoard(){
-        new RankBoardPage(rankBoard).setVisible(true);
-//        if(rankBoard.size() == 0){
-//            System.out.printf("\t\tNo Record!!\n");
-//        }
-//        else{
-//            System.out.printf("===========================================================\n");
-//            System.out.printf("\n\t\tRanking Board\n\n");
-//            System.out.printf("===========================================================\n");
-//            
-//            for(int i = 0; i < rankBoard.size(); i++){
-//                System.out.printf("\t\tRound %d\n", i + 1);
-//                
-//                Vector<RankRecord> record = rankBoard.get(i);
-//                
-//                for(int j = 0; j < record.size(); j++){
-//                	if( record.get(j).isTie() ){
-//                		System.out.printf("\t\tTied!!! NO WINNER :(\n");
-//                	}
-//                	else{
-//                		System.out.printf("\t\tWinner:\t%s\n", record.get(j).getWinnerName());
-//                        System.out.printf("\t\tScore:\t%d\n", record.get(j).getWinnerScore());
-//                	}
-//                }
-//                
-//                System.out.printf("-----------------------------------------------------------\n");
-//            }
-//        }
+    	String str = prb();
+    	RankBoardPage rp = new RankBoardPage(str);
+    	rp.setVisible(true);
+    }
+    
+    //get output rank record for textArea field
+    public String prb(){
+    	String result = "";
+    	
+    	if(rankBoard.size() == 0){
+            result += "\t\tNo Record!!\n";
+        }
+       
+        else{
+            result += "===========================================================\n";
+            result += "\n\t\tRanking Board\n\n";
+            result += "===========================================================\n";
+            
+            for(int i = 0; i < rankBoard.size(); i++){
+                result += "\t\tRound " + (i + 1) + "\n";
+                
+                Vector<RankRecord> record = rankBoard.get(i);
+                
+                for(int j = 0; j < record.size(); j++){
+                	if( record.get(j).isTie() ){
+                		result += "\t\tTied!!! NO WINNER :(\n";
+                	}
+                	else{
+                		result += "\t\tWinner:\t" + record.get(j).getWinnerName() + "\n";
+                		result += "\t\tScore:\t" + record.get(j).getWinnerScore() + "\n";
+                	}
+                }
+                
+                result += "------------------------------------------------------------------------------------------------------\n";
+            }
+        }
+        return result;
     }
     
     public int size(){
